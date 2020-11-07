87f379479e0a05e55449fa54bbde6604f029d8a4
Saurish Kar <saurishkar@outlook.com>
2020-11-07 15:00:50 +0530

    Basic setup (#1)

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
@@ -9,12 +10,15 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>I am beginning to learn Next.js</p>
         <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website - you’ll be building a site like this in{" "}
+          <Link href="/posts/first-post">
+            <a>our Next.js tutorial</a>
+          </Link>
+          .)
         </p>
       </section>
     </Layout>
-  )
+  );
 }
